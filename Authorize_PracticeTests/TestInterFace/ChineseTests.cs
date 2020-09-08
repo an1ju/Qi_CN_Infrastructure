c58c1ed18d55eba6f899bc3fec69d327f5d39dd7
QiZhuhua <an1ju@163.com>
2020-09-08 09:26:03 +0800

    乱改。接口用不好InterFace

---
+++ Authorize_PracticeTests/TestInterFace/ChineseTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Authorize_Practice.TestInterFace;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authorize_Practice.TestInterFace.Tests
+{
+    [TestClass()]
+    public class ChineseTests
+    {
+        [TestMethod()]
+        public void LanguageTest()
+        {
+            
+        }
+    }
+}
